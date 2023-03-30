480d32da73bf29c547c8f2e42e788c292fa6b825
AntonioHristov <ahristov@evicertia.com>
2023-03-30 17:32:54 +0200

    menos numeros magicos y multiplicaciones, divisiones y raices en servidor

--- evicalc.server/Controllers/CalculatorController.cs
+++ evicalc.server/Controllers/CalculatorController.cs
@@ -18,14 +18,19 @@ namespace evicalc.server.Controllers
         [HttpPost]
         public IActionResult Add(AddRequest request)
         {
+            const double digitsNull = 0;
+            const double minimumDigits = 2;
+            double numberDigits = request.Addens.Count();
+
+
             //return BadRequest("Hello");
 
             //if (request.Addens == null)
-            if (request.Addens.Count() == 0)
+            if (numberDigits == digitsNull)
             {
                 return BadRequest("Addens cannot be null");
             }
-            else if (request.Addens.Count() < 2)
+            else if (numberDigits < minimumDigits)
             {
                 return BadRequest("Addens param requires at least two numbers to do something");
             }
@@ -52,49 +57,65 @@ namespace evicalc.server.Controllers
             */
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         [HttpPost]
         //public SubResponse Sub(SubRequest request)
         public IActionResult Sub(SubRequest request)
         {
-            if (request.Minuend == null)
+            /*
+            if (request.Minuend == null) // Try this
             {
                 return BadRequest("Minuend cannot be null");
             }
-
-
-            return Ok(new SubResponse() { Difference = request.Minuend - request.Subtrahend });
+            */
             //return new SubResponse() { Difference = request.Minuend - request.Subtrahend };
+            return Ok(new SubResponse() { Difference = request.Minuend - request.Subtrahend });
         }
 
+        [HttpPost]
+        public IActionResult Mult(MultRequest request)
+        {
+            const double digitsNull = 0;
+            const double minimumDigits = 2;
+
+            double numberDigits = request.Factors.Count();
+            double[] arrayIenumerable = request.Factors.ToArray();
+            double result = 1; // Because if this is 0 or not exist, the result always will be 0 after the operation
+
+
+            if (numberDigits == digitsNull)
+            {
+                return BadRequest("Factors cannot be null");
+            }
+            else if (numberDigits < minimumDigits)
+            {
+                return BadRequest("Factors param requires at least two numbers to do something");
+            }
+
+            foreach (var item in arrayIenumerable)
+            {
+                result *= item;
+            }
+
+            return Ok(new MultResponse() { Product = result });
+        }
+
+        [HttpPost]
+        public IActionResult Div(DivRequest request)
+        {
+            const double illegalDivisor = 0;
+
+            if(request.Divisor == illegalDivisor)
+            {
+                return BadRequest("The divider can't be 0");
+            }
+
+            return Ok(new DivResponse() { Quotient = request.Dividend / request.Divisor, Remainder = request.Dividend % request.Divisor });
+        }
+
+        [HttpPost]
+        public IActionResult Sqrt(SqrtRequest request)
+        {
+            return Ok(new SqrtResponse() { Square = Math.Sqrt(request.Number) });
+        }
     }
 }
